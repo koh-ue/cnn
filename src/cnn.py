e7a340d913dc16dfb1e740d1357b6634181d813b
naboo.koh <koh@uehara.to>
2023-06-15 02:57:48 +0900

    gpu

--- src/cnn.py
+++ src/cnn.py
@@ -200,7 +200,7 @@ if __name__ == '__main__':
 
             print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
             if phase == 'train':
-                print(type(epoch_acc))
+                print(epoch_acc)
                 train_losses.append(epoch_loss.numpy())
                 train_accs.append(epoch_acc)
             else:
